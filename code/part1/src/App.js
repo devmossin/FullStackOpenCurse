58c6fe8872b3fa393ee9a2bc07294dce6bc8934b
Pietro Mossin Araújo <dev.mossin@gmail.com>
2026-03-19 18:35:31 -0300

    Estudos dentro da part1 a

--- code/part1/src/App.js
+++ code/part1/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const Hello = (props) => {
+  console.log(props)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>
+        Olá {props.nome}, você tem {props.idade} anos
+      </p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const nome = 'Mossin'
+  const idade = 23
+
+  return (
+    <div>
+      <h1>Olá a todos!</h1>
+
+      <Hello nome = 'Pietro' idade = {12 + 1}/>
+      <Hello nome = {nome} idade = {idade}/>
+    </div>
+  )
+}
+
+export default App
