517a897344c5f6139f3b0a5fd1ec2b18f5954fa9
Pietro Mossin Araújo <dev.mossin@gmail.com>
2026-03-25 19:39:49 -0300

    Exercise 1.13 todo

---
+++ exercises/part1/anecdotes/src/App.js
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+const Button = props => {
+  return (
+    <button onClick = {props.handleClick}>
+      next anecdotes
+    </button>
+  )
+}
+
+const App = () => {
+  const anecdotes = [
+    'Se fazer algo dói, faça isso com mais frequência.',
+    'Contratar mão de obra para um projeto de software que já está atrasado, faz com que se atrase mais ainda!',
+    'Os primeiros 90% do código correspondem aos primeiros 10% do tempo de desenvolvimento... Os outros 10% do código correspondem aos outros 90% do tempo de desenvolvimento.',
+    'Qualquer tolo escreve código que um computador consegue entender. Bons programadores escrevem código que humanos conseguem entender.',
+    'Otimização prematura é a raiz de todo o mal.',
+    'Antes de mais nada, depurar é duas vezes mais difícil do que escrever o código. Portanto, se você escrever o código da forma mais inteligente possível, você, por definição, não é inteligente o suficiente para depurá-lo.',
+    'Programar sem o uso extremamente intenso do console.log é o mesmo que um médico se recusar a usar raio-x ou testes sanguíneos ao diagnosticar pacientes.',
+    'A única maneira de ir rápido é ir bem.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+
+  const updateAnecdote = () => {
+    const min = 0
+    const max = 7
+    const randomNumber = Math.floor(Math.random() * (max - min) + min)
+    console.log(randomNumber)
+    setSelected(randomNumber)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br></br>
+      <Button handleClick = {updateAnecdote}/>
+    </div>
+  )
+}
+
+export default App
